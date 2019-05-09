2327dc6816e41f2291597b2996c8c24949546c8b
hongshuo <bugmaker@fastmail.com>
2019-05-09 11:39:13 +0800

    108

--- 108ConvertSortedArraytoBinarySearchTree.h
+++ 108ConvertSortedArraytoBinarySearchTree.h
@@ -8,10 +8,23 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        TreeNode* root = NULL;
-
-        return root;
+        return GenTree(nums,0,nums.size());
     }
-
+    TreeNode* GenTree(vector<int>& nums,int begin,int end)
+    {
+        if(begin == end)
+        {
+            return NULL;
+        }
+        else
+        {
+            int mid = (begin+end)/2;
+            int val = nums[mid];
+            TreeNode* node = new TreeNode(val);
+            node->left = GenTree(nums,begin,mid);
+            node->right = GenTree(nums,mid+1,end);
+            return node;
+        }
+    }
 };
 #endif //LEETCODE_108CONVERTSORTEDARRAYTOBINARYSEARCHTREE_H
