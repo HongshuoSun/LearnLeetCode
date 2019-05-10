8ce6b0d30b050591f1ac6bc2735d99c3bda3c104
hongshuo <bugmaker@fastmail.com>
2019-05-10 18:30:08 +0800

    117

---
+++ 117PopulatingNextRightPointersInEachNodeII.h
@@ -0,0 +1,50 @@
+//
+// Created by 孙宏硕 on 2019-05-10.
+//
+
+#ifndef LEETCODE_117POPULATINGNEXTRIGHTPOINTERSINEACHNODEII_H
+#define LEETCODE_117POPULATINGNEXTRIGHTPOINTERSINEACHNODEII_H
+
+#include "Common.h"
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root == nullptr)
+        {
+            return nullptr;
+        }
+        deque<Node*> currentLevelTree;
+        deque<Node*> nextLevelTree;
+        if(root!= nullptr)
+        {
+            nextLevelTree.push_back(root);
+        }
+        Node* last = nullptr;
+        while(!currentLevelTree.empty() || !nextLevelTree.empty())
+        {
+            if(currentLevelTree.empty())
+            {
+                swap(currentLevelTree,nextLevelTree);
+                last = nullptr;
+                continue;
+            }
+            auto current = currentLevelTree.front();
+            currentLevelTree.pop_front();
+            current->next = last;
+            last = current;
+            if(current->right)
+            {
+                nextLevelTree.push_back(current->right);
+            }
+            if(current->left)
+            {
+                nextLevelTree.push_back(current->left);
+            }
+
+        }
+        return root;
+    }
+};
+
+#endif //LEETCODE_117POPULATINGNEXTRIGHTPOINTERSINEACHNODEII_H
