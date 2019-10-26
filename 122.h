826a59ecace5be8a78cf215df582f2f1a4414f86
hongshuo <shs@hitee.net>
2019-10-26 21:41:20 +0800

    122

--- 122.h
+++ 122.h
@@ -5,21 +5,59 @@
 #ifndef LEETCODE_122_H
 #define LEETCODE_122_H
 #include "Common.h"
+
+/*  overtime
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int length = prices.size();
-        if(length<1){
+        int count = prices.size();
+        if(count<=1){
             return 0;
         }
-        int prefixMin = prices[0];
-        int result = 0;
-        for(int i=1;i<length;i++){
-            int sub = prices[i]-prefixMin;
-            prefixMin = min(prices[i],prefixMin);
-            result = max(result,sub);
+        vector<int> dp(count,0);
+        for(int i=1;i<count;i++){
+            dp[i] = dp[i-1];
+            for(int j=0;j<i;j++){
+                dp[i] = max(dp[i],dp[j]+ maxProfit2(prices.begin()+j,prices.begin()+i+1));
+            }
         }
-        return result;
+        return dp[count-1];
+    }
+    int maxProfit2(const vector<int>::iterator begin,const vector<int>::iterator end)
+    {
+        if(end<=begin){
+            return 0;
+        }
+        int minimum = *begin;
+        int ans = 0;
+        for(auto it = begin;it<end;it++){
+            ans = max(ans,*it-minimum);
+            minimum = min(minimum,*it);
+        }
+        return ans;
+    }
+};*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans= 0;
+        int minimum;
+        int maximum;
+        int i=0;
+        int count = prices.size();
+        while(i<count){
+            while(i<count-1&& prices[i]>prices[i+1])
+                i++;
+            minimum = prices[i];
+            while(i<count-1&&prices[i]<prices[i+1]){
+                i++;
+            }
+            maximum = prices[i];
+            ans += max(0,maximum-minimum);
+        }
+        return ans;
     }
 };
+
 #endif //LEETCODE_122_H
