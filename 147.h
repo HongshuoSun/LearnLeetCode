4c7f70155410c3b1aed68044f2be6c2f168a79b8
Hongshuo <bugmaker@fastmail.com>
2021-07-09 00:13:55 +0800

    147

---
+++ 147.h
@@ -0,0 +1,23 @@
+#ifndef LEETCODE_147_H
+#define LEETCODE_147_H
+#include "Common.h"
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        while(head!=nullptr){
+            ListNode* preNode = dummy;
+            ListNode* cur = preNode->next;
+            while(cur!=nullptr && cur->val< head->val){
+                preNode = cur;
+                cur = cur->next;
+            }
+            preNode->next = head;
+            head = head->next;
+            preNode->next->next = cur;
+        }
+        return dummy->next;
+    }
+    
+};
+#endif
